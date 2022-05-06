2c2b2b753151b2baa69c5a2afc6fb9e7700eb24b
Pedro Ferreira <pedrodif@icloud.com>
2022-05-05 23:32:04 -0300

    Feat/Improve Structure

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,37 +1,20 @@
-const repositoryName = 'unform2';
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'Unform2',
+  description: 'Forms in React',
+  link: 'https://github.com/pedrodif/uploadGitHubExplorer'
+}
 
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
-
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   )
