6bc63c5964ab4dd2fab6f66e3019527f847199b5
Pedro Ferreira <pedrodif@icloud.com>
2022-05-11 15:26:28 -0300

    coneccting api dynamically

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,30 +3,25 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
-const repository = {
-  name: 'Unform2',
-  description: 'Forms in React',
-  link: 'https://github.com/pedrodif/uploadGitHubExplorer'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => setRepositories(data))
 
   }, []);
+
+  console.log(repositories);
 
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   )
